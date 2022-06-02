c922ca6eca1e242575494587e7bd3b601a2444d3
Henry Cai <alaba.havai@gmail.com>
2022-06-02 14:05:39 +0800

    Update Calculate logic

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -22,21 +22,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when(selectedId){
+//    可以使用貓王 ?:    val cost = stringInTextField.toDoubleOrNull() ?: return
+        val cost = stringInTextField.toDoubleOrNull()
+        if(cost ==null || cost == 0.0){
+            displayTip(0.0)
+            return
+        }
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
             R.id.option_twenty_percent   -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
         var tip = tipPercentage * cost
-        val roundUp = binding.roundUpSwitch.isChecked
-        if(roundUp){
+        if(binding.roundUpSwitch.isChecked){
             tip = ceil(tip)
         }
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
         //format diffrent contries currencies
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         //set formated currencies text
-        binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
